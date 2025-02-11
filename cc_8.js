ea201b901a39a9ed81b964e7584e28d89d6ee33c
Gabriela Serrano <gabrielasc0309@gmail.com>
2025-02-10 21:57:42 -0500

    Task 6 - Identifying Large Transactions.

--- cc_8.js
+++ cc_8.js
@@ -67,3 +67,16 @@ function calculateLoanPayment(principal, rate, time) {
 console.log(calculateLoanPayment(1000, 0.05, 2)); // Expected output: "Total Payment: $1100.00"
 console.log(calculateLoanPayment(5000, 0.07, 3)); // Expected output: "Total Payment: $6050.00"
 
+// Task 6 - Identifying Large Transactions
+
+// Array of transactions
+let transactions = [200, 1500, 3200, 800, 2500];
+
+// Higher-order function to filter large transactions
+function filterLargeTransactions(transactions, filterFunction) {
+    return transactions.filter(filterFunction);
+}
+
+// Test cases
+console.log(filterLargeTransactions(transactions, amount => amount > 1000)); 
+// Expected output: [1500, 3200, 2500]
